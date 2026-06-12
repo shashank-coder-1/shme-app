f9238a1e468c15c0a6c43f01c774a1bd8475017d
SHASHAANK KALYANAM <shashaank@Shashaank.local>
2026-06-12 21:08:33 +0530

    prepare railway deployment

--- server/index.ts
+++ server/index.ts
@@ -52,6 +52,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5001, () => {
-  console.log("Server running on port 5001");
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
