bcdd03aceec7c97365ef7e56f38f5eebac3dea0b
SHASHAANK KALYANAM <shashaank@Shashaank.local>
2026-06-12 22:13:23 +0530

    debug socket

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,8 +1,13 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "https://shme-app-production.up.railway.app",
-  {
-    transports: ["websocket", "polling"],
-  }
+  "https://shme-app-production.up.railway.app"
 );
+
+socket.on("connect", () => {
+  console.log("Socket Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.log("Socket Error:", err);
+});
